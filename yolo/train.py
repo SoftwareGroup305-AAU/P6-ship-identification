9367b7990ab21ee69f0e6b1c9d2de62f033c0db9
mLario <mscarp22@student.aau.dk>
2025-03-16 21:49:26 +0100

    Remove unused label loading function from training script

--- yolo/train.py
+++ yolo/train.py
@@ -1,17 +1,2 @@
-import os
-from torch.utils.data import Dataset
-from torchvision.io import read_image
-import pandas as pd
 
-def load_label_yolov8(filename):
-    data = pd.read_csv(filename, delim_whitespace=True, header=None)
-    data.columns = ["class", "center_x", "center_y", "width", "height"]
-    bounding_boxes = []
-    for index, row in data.iterrows():
-        bounding_boxes.append([int(row["class"]), 
-                               float(row["center_x"]), 
-                               float(row["center_y"]), 
-                               float(row["width"]), 
-                               float(row["height"])])
-    return bounding_boxes
 
